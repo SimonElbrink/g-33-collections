f497cabac53da6a5440728f1e55bcac4c1aa7c9a
Simon Elbrink <simon.elbrink@lexicon.se>
2020-12-07 14:07:47 +0100

    Today's lecture

--- src/main/java/se/lexicon/collections/App.java
+++ src/main/java/se/lexicon/collections/App.java
@@ -1,13 +1,69 @@
 package se.lexicon.collections;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+public class App {
+    public static void main(String[] args) {
+
+        //list();
+        //set();
+        map();
+    }
+
+    public static void list() {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        numbers.add(200);
+
+        System.out.println(numbers);
+
+        numbers.add(100);
+
+        numbers.set(0, 9000);
+
+        System.out.println(numbers);
+
+        numbers.remove(0);
+
+        System.out.println(numbers);
+    }
+
+
+    public static void set() {
+
+        HashSet<String> names = new HashSet<>();
+
+        names.add("Ulf");
+        names.add("Simon");
+        names.add("Simon");
+
+        System.out.println("names = " + names);
+
+        System.out.println(names.contains("Eric"));
+
+
+    }
+
+    public static void map() {
+
+        HashMap<String, Integer> accounts = new HashMap<>();
+
+        //Adding
+        accounts.put("Simon@lexicon.se", 100_000);
+
+        //Updating
+        accounts.put("Simon@lexicon.se", 1_000_000);
+
+        accounts.get("Simon@lexicon.se");
+
+
+        System.out.println(accounts.get("Simon@lexicon.se"));
+        accounts.containsKey("Simon@lexicon.se");
+
+        System.out.println("accounts = " + accounts);
+
     }
 }
